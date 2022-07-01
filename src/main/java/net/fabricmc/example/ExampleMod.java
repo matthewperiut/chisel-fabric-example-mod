7294b19a18d5bb84812a1615dce06ffb7f601823
matthewperiut <slaincow@gmail.com>
2022-07-01 18:09:43 -0400

    Chisel Addition Example Mod

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,21 +1,41 @@
 package net.fabricmc.example;
 
+import com.matthewperiut.chisel.block.ChiselGroupLookup;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+// Feel free to remove these imports
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.Identifier;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.Material;
+
+import static com.matthewperiut.chisel.Chisel.CHISEL_GROUP;
+import com.matthewperiut.chisel.block.blocks.EzReg;
+//
+
+public class ExampleMod implements ModInitializer
+{
+	public static final String MODID = "example";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	public static final Block TEST_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
 
 	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
+	public void onInitialize()
+	{
 		LOGGER.info("Hello Fabric world!");
+
+		Identifier test_block = new Identifier(MODID, "test_block");
+		Registry.register(Registry.BLOCK, test_block, TEST_BLOCK);
+		Registry.register(Registry.ITEM, test_block, new BlockItem(TEST_BLOCK, new FabricItemSettings().group(CHISEL_GROUP)));
+
+		ChiselGroupLookup.addItemToGroup("stone", test_block);
 	}
 }
